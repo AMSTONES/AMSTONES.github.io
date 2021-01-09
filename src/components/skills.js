f30189193c5eec5f2d783a3f47a0d36d7fa45c6d
Alexander Stones <amstones@hotmail.co.uk>
2021-01-09 14:15:52 +0000

    added non complete code logos

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-class Skills extends Component {
-
-    render() {
+import Pll from 'react-pll'
+function Skills () {
         return (
             <div className="skills">
                 <h1>Skills</h1>
+                <div className="skill-icons-container">
+                    <Pll className="language-logo" language="html" alt="HTML logo" />
+                    <Pll className="language-logo" language="css" alt="CSS logo" />
+                    <Pll className="language-logo" language="javascript" alt="JavaScript logo" />
+                    <Pll className="language-logo" language="php" alt="PHP logo" />
+                    <img className="language-logo" src="/images/react-logo-small.png"/>
+                </div>
 
             </div>
 
         )
-    }
 }
 
 export default Skills
